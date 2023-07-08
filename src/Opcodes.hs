18a9807bb166b8abec13fc03b6e25278d038be34
paolino <paolo.veronelli@gmail.com>
2023-07-08 17:08:42 +0100

    add bidirectional patterns for nibbles

--- src/Opcodes.hs
+++ src/Opcodes.hs
@@ -25,22 +25,39 @@ nibble1 :: Opcode -> (Nibble, Address)
 nibble1 x =
     (fromIntegral $ x `shiftR` 12, fromIntegral $ x .&. 0x0FFF)
 
+-- >>> N1 0xA 0x28B == 0xA28B
+-- True
+
 pattern N1 :: Nibble -> Address -> Opcode
 pattern N1 x y <- (nibble1 -> (x, y))
+    where
+        N1 x y = fromIntegral x `shiftL` 12 .|. fromIntegral y
 
 nibble2 :: Opcode -> (Nibble, Nibble, Word8)
 nibble2 (N1 x y) =
     (x, fromIntegral $ y `shiftR` 8, fromIntegral $ y .&. 0x00FF)
 
+-- >>> N2 0xA 0x2 0x8B == 0xA28B
+-- True
+
+
 pattern N2 :: Nibble -> Nibble -> Word8 -> Opcode
 pattern N2 x y z <- (nibble2 -> (x, y, z))
+    where
+        N2 x y z = N1 x (fromIntegral y `shiftL` 8 .|. fromIntegral z)
 
 nibble3 :: Opcode -> (Nibble, Nibble, Nibble, Nibble)
 nibble3 (N2 x y z) =
     (x, y, fromIntegral $ z `shiftR` 4, fromIntegral $ z .&. 0x000F)
 
+
+-- >>> N3 0xA 0x2 0x8 0xB == 0xA28B
+-- True
+
 pattern N3 :: Nibble -> Nibble -> Nibble -> Nibble -> Opcode
 pattern N3 x y z w <- (nibble3 -> (x, y, z, w))
+    where
+        N3 x y z w = N2 x y (fromIntegral z `shiftL` 4 .|. fromIntegral w)
 
 decode :: Opcode -> Instruction
 decode ClearScreen' = ClearScreen
