ca6eb924745c72d1426485e39bb2f0fddb4efce5
paolo <paolo.veronelli@gmail.com>
2023-07-13 22:52:04 +0100

    add other roms

--- app/Main.hs
+++ app/Main.hs
@@ -3,22 +3,18 @@ module Main (main) where
 import Control.Monad ((>=>))
 import Data.ByteString (readFile)
 import Graphics (chip8Application)
--- import Programs.Dump (dumpPrograms)
 import Rendering (run)
 import State (bootState)
 import Prelude hiding (readFile)
 
 main :: IO ()
 main = do
-    -- run testApplication
-    -- compile "roms/IBM Logo'.ch8"
-    -- dumpPrograms
-    -- TODO: add program selection in roms directory
-    runFile "roms/IBM Logo.ch8"
-    -- runFile "roms/Subroutine.ch8"
-    -- runFile "roms/3-corax+.ch8"
-    --  runFile "roms/4-flags.ch8"
-    -- runFile "roms/5-quirks.ch8"
+    runFile "roms/1-chip8-logo.ch8"
+    runFile "roms/2-ibm-logo.ch8"
+    runFile "roms/3-corax+.ch8"
+    runFile "roms/4-flags.ch8"
+    runFile "roms/5-quirks.ch8"
+    runFile "roms/6-keypad.ch8"
 
 runFile :: FilePath -> IO ()
 runFile = readFile >=> run . chip8Application . bootState
